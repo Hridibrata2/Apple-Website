a845ed8b2c626e5adcb2463d759b86806f3debd0
Hridibrata Saha <hridibrata@gmail.com>
2026-04-17 15:21:26 +0530

    the slidehsow is finally done

--- apple-website/src/Slideshow/Slideshow.jsx
+++ apple-website/src/Slideshow/Slideshow.jsx
@@ -1,153 +1,125 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { FaPlay, FaPause } from "react-icons/fa";
-// import "./Slideshow.css";
-
-// const Slideshow = () => {
-//   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-//   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
-
-//   const [movies, setMovies] = useState([]);
-//   const [index, setIndex] = useState(0);
-//   const [isPlaying, setIsPlaying] = useState(true);
-
-//   useEffect(() => {
-//     const getMovies = async () => {
-//       try {
-//         const response = await axios.get(
-//           "https://api.themoviedb.org/3/movie/popular",
-//           {
-//             params: {
-//               api_key: API_KEY,
-//               language: "en-US",
-//               page: 1,
-//             },
-//           }
-//         );
-
-//         const topPicks = response.data.results.slice(0, 5);
-
-//         const moviesWithLogos = await Promise.all(
-//           topPicks.map(async (movie) => {
-//             const logoRes = await axios.get(
-//               `https://api.themoviedb.org/3/movie/${movie.id}/images`,
-//               {
-//                 params: { api_key: API_KEY },
-//               }
-//             );
-
-//             const logo = logoRes.data.logos.find(
-//               (l) => l.iso_639_1 === "en"
-//             )?.file_path;
-
-//             return { ...movie, logo };
-//           })
-//         );
-
-//         setMovies(moviesWithLogos);
-//       } catch (error) {
-//         console.error("Failed to fetch data", error);
-//       }
-//     };
-
-//     getMovies();
-//   }, []);
-
-//   useEffect(() => {
-//     if (!isPlaying || movies.length === 0) return;
-
-//     const interval = setInterval(() => {
-//       setIndex((prev) => (prev + 1) % movies.length);
-//     }, 3000);
-
-//     return () => clearInterval(interval);
-//   }, [movies, isPlaying]);
-
-//   const currentMovie = movies[index];
-
-//   return (
-//     <div className="slideshow">
-//       {currentMovie && (
-//         <div className="slide">
-
-//           <img
-//             className="background"
-//             src={`${IMAGE_BASE_URL}${currentMovie.backdrop_path}`}
-//             alt={currentMovie.title}
-//           />
-
-//           <div className="overlay">
-//             {currentMovie.logo ? (
-//               <img
-//                 className="logo"
-//                 src={`${IMAGE_BASE_URL}${currentMovie.logo}`}
-//                 alt="logo"
-//               />
-//             ) : (
-//               <h1>{currentMovie.title}</h1>
-//             )}
-//           </div>
-//         </div>
-//       )}
-
-//       <button
-//         className="play-btn"
-//         onClick={() => setIsPlaying(!isPlaying)}
-//       >
-//         {isPlaying ? <FaPause size={18} /> : <FaPlay size={18} />}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Slideshow;
-
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Slideshow.css"
+import "./Slideshow.css";
 
 const Slideshow = () => {
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
+  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-  const [movie, setMovies] = useState([])
-  const [index, setIndex] = useState(0)
+  const [movies, setMovies] = useState([]);
+  const [index, setIndex] = useState(1);
+  const [transition, setTransition] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(true);
 
-useEffect(() => {
-      const findMovies = async () => {
-        try {
-          const response = await axios.get(
-             "https://api.themoviedb.org/3/movie/popular",
-             {
-              params: {
-                api_key: API_KEY,
-                language: "en-US",
-                page: 1
-              },
-             }
-          )
-
-          const moviesGot =  response.data.results.slice(0,6)
-          setMovies(moviesGot)
-          console.log(moviesGot)
-
-        } catch (error) {
-          console.error("cannot get the data")
+  
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/movie/popular",
+        {
+          params: {
+            api_key: API_KEY,
+            language: "en-US",
+            page: 2,
+          },
         }
-      }
-      findMovies()
-}, [])
+      );
 
-return (
-  <div>
-    {movie.map((movies) => (
-      <img
-        key={movies.id}
-        src={`${IMAGE_BASE_URL}${movies.backdrop_path}`}
-        alt={movies.title}
-      />
-    ))}
-  </div>
-)
-}
-export default Slideshow
+      const data = response.data.results.slice(0, 6);
+      console.log(data)
+      setMovies([data[data.length - 1], ...data, data[0]]);
+    };
+
+    fetchMovies();
+  }, []);
+
+ 
+  useEffect(() => {
+    if (movies.length === 0 || !isPlaying) return;
+
+    const timer = setInterval(() => {
+      setIndex((prev) => prev + 1);
+    }, 4000);
+
+    return () => clearInterval(timer);
+  }, [movies, isPlaying]);
+
+  
+  useEffect(() => {
+    if (movies.length === 0) return;
+
+    if (index === movies.length - 1) {
+      setTimeout(() => {
+        setTransition(false);
+        setIndex(1);
+      }, 700);
+    }
+
+    if (index === 0) {
+      setTimeout(() => {
+        setTransition(false);
+        setIndex(movies.length - 2);
+      }, 800);
+    }
+  }, [index, movies]);
+
+ 
+useEffect(() => {
+  if (!transition) {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        setTransition(true);
+      });
+    });
+  }
+}, [transition]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="carousel-track"
+        style={{
+          transform: `translateX(calc(-${index * 69}% - ${index * 3}px))`,
+          transition: transition ? "transform 0.7s ease-in-out" : "none",
+        }}
+      >
+        {movies.map((movie, i) => (
+          <div className={`slide ${i === index ? "active" : ""}`} key={i}>
+            <img
+              src={`${IMAGE_BASE_URL}${movie.backdrop_path}`}
+              alt={movie.title}
+            />
+
+            {i === index && (
+              <div className="overlay">
+                <button>Stream now</button>
+                <p>
+                  <strong>{movie.title}</strong>
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      
+      <div className="dots">
+        {movies.slice(1, -1).map((_, i) => (
+          <span
+            key={i}
+            className={i + 1 === index ? "active-dot" : ""}
+          ></span>
+        ))}
+      </div>
+
+      <button
+        className="pause-btn"
+        onClick={() => setIsPlaying(!isPlaying)}
+      >
+        {isPlaying ? "❚❚" : "▶"}
+      </button>
+    </div>
+  );
+};
+
+export default Slideshow;
